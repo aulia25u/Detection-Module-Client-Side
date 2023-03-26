f9611de1fd761dc132ebf9167c91511c1662ec59
Aulia Rahman <56797963+aulia25u@users.noreply.github.com>
2023-03-26 16:33:23 +0700

    update Face Detect JS

--- app-facedetect.js
+++ app-facedetect.js
@@ -8,6 +8,7 @@ async function loadModels() {
     "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights";
   await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
   await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
+  await faceapi.nets.faceExpressionNet.loadFromUri(modelPath);
 }
 
 // Start webcam and process frames
@@ -20,22 +21,48 @@ async function startVideo() {
       video.srcObject = stream;
 
       video.addEventListener("play", async () => {
+        const detectionInterval = 100; // Adjust this value to control detection frequency (in milliseconds)
+        const minConfidence = 0.5; // Adjust this value to control detection accuracy (0 to 1)
+
         while (video.readyState === 4) {
           const detections = await faceapi
-            .detectAllFaces(video, new faceapi.SsdMobilenetv1Options())
-            .withFaceLandmarks();
+            .detectAllFaces(
+              video,
+              new faceapi.SsdMobilenetv1Options({ minConfidence })
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions();
 
-          faceCountElement.textContent = detections.length;
+          // Resize video input
+          const inputSize = 224;
+          const resizedResults = faceapi.resizeResults(detections, {
+            width: inputSize,
+            height: inputSize,
+          });
+
+          faceCountElement.textContent = resizedResults.length;
 
           let landmarksText = "";
-          detections.forEach((detection, index) => {
+          resizedResults.forEach((detection, index) => {
             landmarksText += `Face ${index + 1} coordinates: ${JSON.stringify(
               detection.landmarks.positions
             )}\n`;
+
+            // Eye coordinates
+            const leftEye = detection.landmarks.getLeftEye();
+            const rightEye = detection.landmarks.getRightEye();
+            landmarksText += `Face ${
+              index + 1
+            } left eye coordinates: ${JSON.stringify(leftEye)}\n`;
+            landmarksText += `Face ${
+              index + 1
+            } right eye coordinates: ${JSON.stringify(rightEye)}\n`;
           });
           faceLandmarksElement.textContent = landmarksText;
 
-          await new Promise((resolve) => setTimeout(resolve, 100));
+          await new Promise((resolve) =>
+            setTimeout(resolve, detectionInterval)
+          );
         }
       });
     });
