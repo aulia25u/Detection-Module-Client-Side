f8166694952f33b028994b158fe57a478bf66a40
Aulia Rahman <56797963+aulia25u@users.noreply.github.com>
2023-03-26 16:55:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ async function loadModels() {
 }
 
 async function getReferenceImage(url) {
-  const corsProxy = "https://api.allorigins.win/raw?url=";
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
 
   const img = await faceapi.fetchImage(corsProxy + url);
   const detections = await faceapi
