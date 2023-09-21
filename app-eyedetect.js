58f95ca64110895c6279cfeec23b75dccd955bee
Aulia Rahman <56797963+aulia25u@users.noreply.github.com>
2023-09-21 15:54:38 +0700

    tes

--- app-eyedetect.js
+++ app-eyedetect.js
@@ -1,96 +1,87 @@
 const video = document.getElementById("video");
+const overlayCanvas = document.getElementById("overlay");
+const overlayContext = overlayCanvas.getContext("2d");
 const faceCountElement = document.getElementById("face-count");
 const faceLandmarksElement = document.getElementById("face-landmarks");
-const overlayCanvas = document.getElementById("overlay");
-const overlayContext = overlayCanvas.getContext('2d');
 
-// Load face-api.js models
 async function loadModels() {
-  const modelPath = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights";
-  await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
-  await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
-  await faceapi.nets.faceExpressionNet.loadFromUri(modelPath);
-}
-
-function mapLandmarkCoordinates(landmarks) {
-  const mappedLandmarks = {};
-
-  // Face landmark names
-  const landmarkNames = ["leftEye", "rightEye"];
-
-  // Starting and ending indices for each landmark
-  const landmarkIndices = [
-    [36, 41],
-    [42, 47]
-  ];
-
-  landmarkNames.forEach((landmarkName, index) => {
-    const [start, end] = landmarkIndices[index];
-    mappedLandmarks[landmarkName] = landmarks.slice(start, end + 1);
-  });
-
-  return mappedLandmarks;
+    const modelPath = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights";
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
+    await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
+    await faceapi.nets.faceExpressionNet.loadFromUri(modelPath);
 }
 
 function drawEyePosition(video, mappedLandmarks) {
-    // First, draw the video frame
     overlayContext.drawImage(video, 0, 0, overlayCanvas.width, overlayCanvas.height);
-  
-    // Calculate and draw the center for each eye
+
     ["leftEye", "rightEye"].forEach((eye) => {
-      const eyePositions = mappedLandmarks[eye];
-      const x = eyePositions.reduce((sum, pos) => sum + pos._x, 0) / eyePositions.length;
-      const y = eyePositions.reduce((sum, pos) => sum + pos._y, 0) / eyePositions.length;
-  
-      overlayContext.beginPath();
-      overlayContext.arc(x, y, 5, 0, 2 * Math.PI);
-      overlayContext.fillStyle = "red";
-      overlayContext.fill();
-      overlayContext.closePath();
+        const eyePositions = mappedLandmarks[eye];
+        overlayContext.beginPath();
+        eyePositions.forEach((pos, idx) => {
+            if (idx === 0) {
+                overlayContext.moveTo(pos._x, pos._y);
+            } else {
+                overlayContext.lineTo(pos._x, pos._y);
+            }
+        });
+        overlayContext.closePath();
+        overlayContext.strokeStyle = "red";
+        overlayContext.stroke();
     });
-  }
-  
+}
 
-// Start webcam and process frames
+function displayEyePositions(mappedLandmarks) {
+    const eyeCoordinates = {
+        leftEye: null,
+        rightEye: null
+    };
+
+    ["leftEye", "rightEye"].forEach(eye => {
+        const positions = mappedLandmarks[eye];
+        const x = positions.reduce((sum, pos) => sum + pos._x, 0) / positions.length;
+        const y = positions.reduce((sum, pos) => sum + pos._y, 0) / positions.length;
+        eyeCoordinates[eye] = { x, y };
+    });
+
+    faceLandmarksElement.innerHTML = `
+        Left Eye: X: ${eyeCoordinates.leftEye.x.toFixed(2)}, Y: ${eyeCoordinates.leftEye.y.toFixed(2)}<br>
+        Right Eye: X: ${eyeCoordinates.rightEye.x.toFixed(2)}, Y: ${eyeCoordinates.rightEye.y.toFixed(2)}
+    `;
+}
+
 async function startVideo() {
-  await loadModels();
+    await loadModels();
 
-  await navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-      video.srcObject = stream;
+    await navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+            video.srcObject = stream;
+            video.addEventListener("play", async () => {
+                while (video.readyState === 4) {
+                    const detections = await faceapi
+                        .detectAllFaces(video)
+                        .withFaceLandmarks();
 
-      video.addEventListener("play", async () => {
-        const detectionInterval = 100; // Adjust this value to control detection frequency (in milliseconds)
-        const minConfidence = 0.5; // Adjust this value to control detection accuracy (0 to 1)
+                    const resizedResults = faceapi.resizeResults(detections, {
+                        width: video.width,
+                        height: video.height
+                    });
 
-        while (video.readyState === 4) {
-          const detections = await faceapi
-            .detectAllFaces(
-              video,
-              new faceapi.SsdMobilenetv1Options({ minConfidence })
-            )
-            .withFaceLandmarks();
+                    faceCountElement.textContent = resizedResults.length;
+                    resizedResults.forEach((detection) => {
+                        const mappedLandmarks = {
+                            leftEye: detection.landmarks.getLeftEye(),
+                            rightEye: detection.landmarks.getRightEye()
+                        };
 
-          // Resize video input
-          const inputSize = 224;
-          const resizedResults = faceapi.resizeResults(detections, {
-            width: inputSize,
-            height: inputSize,
-          });
+                        drawEyePosition(video, mappedLandmarks);
+                        displayEyePositions(mappedLandmarks);
+                    });
 
-          faceCountElement.textContent = resizedResults.length;
-          resizedResults.forEach((detection) => {
-            const mappedLandmarks = mapLandmarkCoordinates(detection.landmarks.positions);
-            drawEyePosition(video, mappedLandmarks);
-          });
-
-          await new Promise((resolve) =>
-            setTimeout(resolve, detectionInterval)
-          );
-        }
-      });
-    });
+                    await new Promise((resolve) => setTimeout(resolve, 100));
+                }
+            });
+        });
 }
 
 startVideo();
